343f768277d1b381d8f77215f8abdf2fe4a57654
linbin <i@teenet.me>
2019-05-30 11:02:59 -0400

    new

--- src/urlEncodeQueryHandle.js
+++ src/urlEncodeQueryHandle.js
@@ -3,27 +3,30 @@
  * @param requests
  * @param response
  */
-exports.get = (requests,response) => {
+exports.get = async (requests,response) => {
     // Parsing parameters
 
     let dataset = requests.params.dataset.split(".");
     if (dataset[1] === undefined) {
         response.send("Suffix needed");
-    }else if (requests._parsedUrl.query === null) {
+        return null;
+    }
+    if (requests._parsedUrl.query === null) {
         response.send("Parameters needed");
+        return null;
+    }
 
-        // For JSOn
-    }else if (dataset[1].toLowerCase() === "json") {
-        (async function f() {
-            let result = await require('./query/queryResultToJSON').queryToJson(dataset[0],requests._parsedUrl.query);
-            response.send(result);
-        })()
+    // For JSON
+    if (dataset[1].toLowerCase() === "json") {
+        let result = await require('./query/queryResultToJSON').queryToJson(dataset[0],requests._parsedUrl.query);
+        response.send(result);
+        return null;
+    }
 
-        // For CSV
-    }else if (dataset[1].toLowerCase() === "csv") {
-        (async function f() {
-            let result = await require('./query/queryResultToCSV').resultToCSV(dataset[0],requests._parsedUrl.query);
-            response.send(result);
-        })();
+    // For CSV
+    if (dataset[1].toLowerCase() === "csv") {
+        let result = await require('./query/queryResultToCSV').resultToCSV(dataset[0],requests._parsedUrl.query);
+        response.send(result);
+        return null;
     }
 };
